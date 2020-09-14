b61f6d99ade501c831bef0eaec1c5ec0d74d2788
Reina Kobayashi <kinokodaizukinako@gmail.com>
2020-09-14 15:08:35 +0900

    Initial Commit

---
+++ TechMon/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TechMon
+//
+//  Created by 小林玲衣奈 on 2020/09/14.
+//  Copyright © 2020 Reina Kobayashi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
