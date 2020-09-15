e06b5d7056f4164e095a422b6c884ce9093a0794
Reina Kobayashi <kinokodaizukinako@gmail.com>
2020-09-15 15:52:49 +0900

    テクモン

---
+++ TechMon/LobbyViewController.swift
@@ -0,0 +1,101 @@
+//
+//  LobbyViewController.swift
+//  TechMon
+//
+//  Created by 小林玲衣奈 on 2020/09/14.
+//  Copyright © 2020 Reina Kobayashi. All rights reserved.
+//
+
+import UIKit
+
+class LobbyViewController: UIViewController {
+    
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var staminaLabel: UILabel!
+    
+    let techMonManager = TechMonManager.shared
+    
+    var player: Character!
+    var enemy: Character!
+    var stamina: Int = 100
+    var staminaTimer: Timer!
+    
+    //アプリが起動した時一度だけ呼ばれる
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //キャラクターの読み込み
+        player = techMonManager.player
+        enemy = techMonManager.enemy
+        
+    //UIの設定
+        nameLabel.text = "勇者"
+        staminaLabel.text = "\(stamina) / 100"
+        
+    //タイマーの設定
+        staminaTimer = Timer.scheduledTimer(
+            timeInterval: 3,
+            target: self,
+            selector: #selector(updateStaminaValue),
+            userInfo: nil,
+            repeats: true)
+        staminaTimer.fire()
+        
+        
+
+        // Do any additional setup after loading the view.
+    }
+    
+    //ロビー画面が見えるようになる時に呼ばれる
+    override func viewWillAppear(_ animated: Bool) {
+        
+        super.viewWillAppear(animated)
+        techMonManager.playBGM(fileName: "lobby")
+    }
+    
+    //ロビー画面が見えなくなる時に呼ばれる
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        techMonManager.stopBGM()
+    }
+    
+    @IBAction func toBattle(){
+        
+        //スタミナが50以上あればスタミナ50を消費して戦闘画面へ
+        if stamina >= 50{
+            stamina -= 50
+            staminaLabel.text = "\(stamina) / 100"
+            performSegue(withIdentifier: "toBattle", sender: nil)
+        } else {
+            let alert = UIAlertController(
+            title: "バトルに行けません",
+            message: "スタミナを貯めてください",
+            preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            present(alert, animated: true, completion: nil)
+        }
+        
+        }
+    
+    //スタミナの回復
+    @objc func updateStaminaValue(){
+        if stamina < 100{
+            stamina += 1
+            staminaLabel.text =  "\(stamina) / 100"
+        }
+    }
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
